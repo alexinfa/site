c3c154efa4a1296cd3534f0858efa580eaac6afd
alexinfa <infa1983@gmail.com>
2026-01-29 16:11:20 +0100

    set structure

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 - "npx gulp" per far partire il server locale con il watch
 - "npm run new-page -- nome-pagina" per creare automaticamente nuove pagine partendo dal template base
 - "npm run new-component -- nome-componente" per creare automaticamente il file vuoto .njk nella cartella components (oppure sottocartella/nome-componente)
+
+
+- estendere il template: {% extends "base.njk" %}
+- includere il componente/pagina {% include "header.njk" %} 
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,33 @@
-console.log('test');
+document.addEventListener("DOMContentLoaded", function () {
+
+    var showMenu = document.querySelector(".btn-menu");
+    showMenu.addEventListener("click", function (e) {
+        let menuContent = document.querySelector(".navigation");
+        menuContent.classList.toggle("show");
+        showMenu.classList.toggle("open");
+        document.body.classList.toggle("noscroll");  
+    });
+
+    var scrollPageBtn = document.querySelector(".btn-large");
+    window.addEventListener("scroll", function () {
+        let scroll = window.scrollY;
+        if (scroll > 1) {
+            scrollPageBtn.classList.add("move");
+            scrollPageBtn.href = "#top";
+        } else {
+            scrollPageBtn.classList.remove("move");
+            scrollPageBtn.href = "#content";
+        }
+    });
+
+    // // carousel
+    // $('.one-time').slick({
+    //     dots: false,
+    //     infinite: true,
+    //     speed: 300,
+    //     slidesToShow: 1,
+    //     adaptiveHeight: true
+    // });
+
+    console.log("JS loaded");
+});
